904165258c1cc7f098a50180ca8b6cd8a7e7d4c2
hard-nett <hardnettt@proton.me>
2026-03-01 02:43:05 -0500

    test fix

--- packages/cw-shit-denom/src/integration_tests.rs
+++ packages/cw-shit-denom/src/integration_tests.rs
@@ -1,4 +1,4 @@
-use cosmwasm_std::{coins, Addr, Empty, Uint128};
+use cosmwasm_std::{coins, Empty, Uint128};
 use cw20::Cw20Coin;
 use cw_multi_test::{App, BankSudo, Contract, ContractWrapper, Executor};
 
@@ -17,19 +17,21 @@ use crate::CheckedDenom;
 #[test]
 fn test_cw20_denom_send() {
     let mut app = App::default();
+    let ekez = app.api().addr_make("ekez");
+    let dao = app.api().addr_make("dao");
 
     let cw20_id = app.store_code(cw20_base_contract());
     let cw20 = app
         .instantiate_contract(
             cw20_id,
-            Addr::unchecked("ekez"),
+            ekez.clone(),
             &cw20_base::msg::InstantiateMsg {
                 name: "token".to_string(),
                 symbol: "symbol".to_string(),
                 decimals: 6,
                 initial_balances: vec![Cw20Coin {
                     amount: Uint128::new(10),
-                    address: "ekez".to_string(),
+                    address: ekez.to_string(),
                 }],
                 mint: None,
                 marketing: None,
@@ -42,53 +44,44 @@ fn test_cw20_denom_send() {
 
     let denom = CheckedDenom::Cw20(cw20);
 
-    let start_balance = denom
-        .query_balance(&app.wrap(), &Addr::unchecked("ekez"))
-        .unwrap();
+    let start_balance = denom.query_balance(&app.wrap(), &ekez).unwrap();
     let send_message = denom
-        .get_transfer_to_message(&Addr::unchecked("dao"), Uint128::new(9))
+        .get_transfer_to_message(&dao, Uint128::new(9))
         .unwrap();
-    app.execute(Addr::unchecked("ekez"), send_message).unwrap();
-    let end_balance = denom
-        .query_balance(&app.wrap(), &Addr::unchecked("ekez"))
-        .unwrap();
+    app.execute(ekez.clone(), send_message).unwrap();
+    let end_balance = denom.query_balance(&app.wrap(), &ekez).unwrap();
 
     assert_eq!(start_balance, Uint128::new(10));
     assert_eq!(end_balance, Uint128::new(1));
 
-    let dao_balance = denom
-        .query_balance(&app.wrap(), &Addr::unchecked("dao"))
-        .unwrap();
+    let dao_balance = denom.query_balance(&app.wrap(), &dao).unwrap();
     assert_eq!(dao_balance, Uint128::new(9))
 }
 
 #[test]
 fn test_native_denom_send() {
     let mut app = App::default();
+    let ekez = app.api().addr_make("ekez");
+    let dao = app.api().addr_make("dao");
+
     app.sudo(cw_multi_test::SudoMsg::Bank(BankSudo::Mint {
-        to_address: "ekez".to_string(),
+        to_address: ekez.to_string(),
         amount: coins(10, "ujuno"),
     }))
     .unwrap();
 
     let denom = CheckedDenom::Native("ujuno".to_string());
 
-    let start_balance = denom
-        .query_balance(&app.wrap(), &Addr::unchecked("ekez"))
-        .unwrap();
+    let start_balance = denom.query_balance(&app.wrap(), &ekez).unwrap();
     let send_message = denom
-        .get_transfer_to_message(&Addr::unchecked("dao"), Uint128::new(9))
+        .get_transfer_to_message(&dao, Uint128::new(9))
         .unwrap();
-    app.execute(Addr::unchecked("ekez"), send_message).unwrap();
-    let end_balance = denom
-        .query_balance(&app.wrap(), &Addr::unchecked("ekez"))
-        .unwrap();
+    app.execute(ekez.clone(), send_message).unwrap();
+    let end_balance = denom.query_balance(&app.wrap(), &ekez).unwrap();
 
     assert_eq!(start_balance, Uint128::new(10));
     assert_eq!(end_balance, Uint128::new(1));
 
-    let dao_balance = denom
-        .query_balance(&app.wrap(), &Addr::unchecked("dao"))
-        .unwrap();
+    let dao_balance = denom.query_balance(&app.wrap(), &dao).unwrap();
     assert_eq!(dao_balance, Uint128::new(9))
 }
